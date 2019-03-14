86c3a525bab2e02d67460463f566ad354539584e
pedronicolas <pedro.nicolas.perez96@gmail.com>
2019-03-14 10:53:55 +0100

    añadida rama Pedro forked

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [];
 /*
-  Hola esto es una prueba2
+  Hola esto es una prueba2 de forked
 */
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
