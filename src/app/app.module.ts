9455010b64b623bf3ff202508aaf234c54b96e65
pedronicolas <pedro.nicolas.perez96@gmail.com>
2019-03-14 11:04:14 +0100

    anyadidos componentes de trabajo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PostsComponent } from './posts/posts.component';
+import { ShowpostsComponent } from './showposts/showposts.component';
+import { RegistroComponent } from './registro/registro.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    PostsComponent,
+    ShowpostsComponent,
+    RegistroComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/showposts/showposts.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-showposts',
+  templateUrl: './showposts.component.html',
+  styleUrls: ['./showposts.component.scss']
+})
+export class ShowpostsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
